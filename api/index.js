49c0793bf3345c0105d912292206017394da18aa
Abhishek Bansal <130365962+abhishekbansalgemini@users.noreply.github.com>
2023-07-21 17:09:21 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://kajd.onrender.com/",
+    origin: "https://kajd.onrender.com",
   })
 );
 
